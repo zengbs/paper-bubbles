9ac8ebe5ecff5f79e6510550d4cccb6338af3502
Po-Hsun Tseng <zengbs@gmail.com>
2022-01-05 00:29:34 +0800

    update

--- figures/combine_xRay.sh
+++ figures/combine_xRay.sh
@@ -2,5 +2,4 @@ convert fig__x-ray-profile-0.8keV-000.png -resize 1334x fig__x-ray-profile-0.8ke
 
 convert -trim fig__xray_0.8keV_angle_000.png fig__x-ray-profile-0.8keV-000-resize.png -smush 10 fig__xRay.png
 
-
 rm fig__x-ray-profile-0.8keV-000-resize.png
