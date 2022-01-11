9ec3d85ba5c1489afeb6a9048d8f8538bddfd872
Po-Hsun Tseng <zengbs@gmail.com>
2022-01-11 14:31:52 +0800

    update

--- figures/combine_xRay.sh
+++ figures/combine_xRay.sh
@@ -1,8 +1,1 @@
-convert fig__xray_0.8keV_observed.png -resize 1100x fig__xray_0.8keV_observed-resize.png
-
-convert -size 1320x1200 \
-        -page 0x0 fig__xray_0.8keV_observed-resize.png  \
-        -page 50x50 fig__xray_0.8keV_angle_000.png  \
-        -layers flatten  fig__xRayMap.png
-
-rm fig__xray_0.8keV_observed-resize.png
+convert fig__xray_0.8keV_observed.png -resize x570 fig__xray_0.8keV_observed-resize.png
